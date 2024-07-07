cbdedb345ebfd94394d2e0293f734b2f34fb3b64
AJ-uchihaa <ajaj32868@gmail.com>
2024-07-06 22:28:48 -0700

    fitch api added

--- add.js
+++ add.js
@@ -1,9 +1,13 @@
-const i = document.querySelector('.input');
-const btn = document.querySelector('.btn');
-btn.addEventListener("click",()=>{
-    aj()
-})
-function aj(){
-    alert("hii")
+let url = 'https://meowfacts.herokuapp.com';
+
+async function aj(){
+   try {
+    let q = await fetch(url);
+    let w = await q.json();
+    console.log(w)
+   } catch (error) {
+      alert('error')
+   }
+    
 }
 aj();
